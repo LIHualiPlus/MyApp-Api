77e55741c2ab2e295bf4a857f999539ecd89834a
lihl <lihl@1handcar.com>
2018-08-09 09:35:38 +0800

    1

---
+++ MyApp.Api/MyApp.Service/Log/LookLogService.cs
@@ -0,0 +1,28 @@
+﻿using MyApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Service.Log
+{
+    public class LookLogService
+    {
+
+        public MyAppApiResult<bool> AddLog( MyApp_LookLog log)
+        {
+            var res = new MyAppApiResult<bool>();
+            try
+            {
+                 
+
+            }
+            catch (Exception e)
+            {
+                res.AddError(e.Message);
+            }
+            return res;
+        }
+    }
+}
